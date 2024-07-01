9cd1fd2e252acd6b682abcaa5c2d54dcaf04e3d9
lurumad <luisruizpavon@gmail.com>
2024-07-01 18:17:55 +0200

    refactor: dependency injection

--- src/main/java/com/arolla/legacy/testing/quotebot/Application.java
+++ src/main/java/com/arolla/legacy/testing/quotebot/Application.java
@@ -1,8 +1,17 @@
 package com.arolla.legacy.testing.quotebot;
 
+import com.arolla.legacy.testing.quotebot.infrastructure.MarketStudyVendorAdapter;
+import com.arolla.legacy.testing.quotebot.infrastructure.QuotePublisherAdapter;
+import com.arolla.legacy.testing.quotebot.infrastructure.SystemClock;
+import com.arolla.legacy.testing.thirdparty.quotebot.MarketStudyVendor;
+
 public class Application {
     public static void main(String[] args) {
-        AutomaticQuoteBot bot = new AutomaticQuoteBot();
+        AutomaticQuoteBot bot = new AutomaticQuoteBot(
+                new MarketStudyVendorAdapter(new MarketStudyVendor()),
+                new QuotePublisherAdapter(),
+                new SystemClock()
+        );
         bot.sendAllQuotes("FAST");
     }
 
--- src/main/java/com/arolla/legacy/testing/quotebot/AutomaticQuoteBot.java
+++ src/main/java/com/arolla/legacy/testing/quotebot/AutomaticQuoteBot.java
@@ -1,17 +1,32 @@
 package com.arolla.legacy.testing.quotebot;
 
+import com.arolla.legacy.testing.quotebot.domain.Clock;
 import com.arolla.legacy.testing.quotebot.infrastructure.MarketStudyVendorAdapter;
 import com.arolla.legacy.testing.quotebot.infrastructure.QuotePublisherAdapter;
 import com.arolla.legacy.testing.quotebot.infrastructure.SystemClock;
-import com.arolla.legacy.testing.thirdparty.quotebot.MarketStudyVendor;
 
 public class AutomaticQuoteBot {
+
+    private final MarketStudyVendorAdapter marketDataRetriever;
+    private final QuotePublisherAdapter publisher;
+    private final Clock clock;
+
+    public AutomaticQuoteBot(
+            MarketStudyVendorAdapter marketDataRetriever,
+            QuotePublisherAdapter publisher,
+            Clock clock
+    ) {
+        this.marketDataRetriever = marketDataRetriever;
+        this.publisher = publisher;
+        this.clock = clock;
+    }
+
     public void sendAllQuotes(String mode) {
         var blogs = AdSpace.getAdSpaces();
         var auctionTask = new BlogAuctionTask(
-                new MarketStudyVendorAdapter(new MarketStudyVendor()),
-                new QuotePublisherAdapter(),
-                new SystemClock()
+                marketDataRetriever,
+                publisher,
+                clock
         );
         for (String blog : blogs) {
             auctionTask.priceAndPublish(blog, mode);
