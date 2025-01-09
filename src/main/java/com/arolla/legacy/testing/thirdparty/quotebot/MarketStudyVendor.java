c75bbaee0da806e357ab61bceee92a47ac9de061
lurumad <luisruizpavon@gmail.com>
2025-01-09 08:19:44 +0100

    wip: remove jpanel

--- src/main/java/com/arolla/legacy/testing/thirdparty/quotebot/MarketStudyVendor.java
+++ src/main/java/com/arolla/legacy/testing/thirdparty/quotebot/MarketStudyVendor.java
@@ -2,14 +2,11 @@ package com.arolla.legacy.testing.thirdparty.quotebot;
 
 import java.util.Random;
 
-import javax.swing.JOptionPane;
-
 public class MarketStudyVendor {
 
 	public double averagePrice(String blog) {
 		if (System.getenv("license") == null) {
-			JOptionPane.showMessageDialog(null, "Missing license !",
-					"Stupid license", JOptionPane.WARNING_MESSAGE);
+			System.out.println("Missing license !");
 			throw new RuntimeException("");
 		}
 		return ((double) blog.hashCode() * (new Random()).nextDouble());
---
+++ src/test/java/com/arrolla/legacy/testing/DummyTest.java
@@ -0,0 +1,11 @@
+package com.arrolla.legacy.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    public void dummy() {
+        assert true;
+    }
+}
